2f7c9fa9aeced664a5a439105d2550d3897d208c
Richard Le <richardle1997@gmail.com>
2019-05-06 02:54:55 -0700

    Line tracing and Collision Mask/Queries

--- Source/RoomEscape/Private/Grabber.cpp
+++ Source/RoomEscape/Private/Grabber.cpp
@@ -2,6 +2,7 @@
 
 #include "Grabber.h"
 #include "Engine/World.h"
+#include "Runtime/Engine/Public/DrawDebugHelpers.h"
 #define OUT
 
 // Sets default values for this component's properties
@@ -35,9 +36,23 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 	FRotator PlayerViewPointRotation;
 	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(OUT PlayerViewPointLocation, OUT PlayerViewPointRotation);
 	//Log What we get
-	UE_LOG(LogTemp, Warning, TEXT("Location: %s Rotation: %s"), *PlayerViewPointLocation.ToString(), *PlayerViewPointRotation.ToString());
-	//Ray-cast out to reach distance
+	//UE_LOG(LogTemp, Warning, TEXT("Location: %s Rotation: %s"), *PlayerViewPointLocation.ToString(), *PlayerViewPointRotation.ToString());
+	//
+	
+	FVector LineTraceEnd = PlayerViewPointLocation + PlayerViewPointRotation.Vector() * Reach;
+	//Draw a red trace in the world to visualize.
+	DrawDebugLine(GetWorld(), PlayerViewPointLocation, LineTraceEnd, FColor(255, 0, 0), false, 0.f, 0.f, 10.f);
+
+
+	//Setup query parameters
+	FCollisionQueryParams TraceParameters(FName(TEXT("")), false, GetOwner());
+	FHitResult LineTraceHit;
+	GetWorld()->LineTraceSingleByObjectType(OUT LineTraceHit, PlayerViewPointLocation, LineTraceEnd, FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody), TraceParameters);
 
 	//See what we hit
+	if (LineTraceHit.GetActor()) {
+		FString actorName = LineTraceHit.GetActor()->GetName();
+		UE_LOG(LogTemp, Warning, TEXT("Grabbing: %s"), *actorName);
+	}
 }
 
